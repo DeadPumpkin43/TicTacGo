03e8fd71c3eba23f02871ae46ecc636de76821f3
DeadPumpkin <masonyeets234@gmail.com>
2024-10-14 20:46:29 -0400

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex flex-col my-auto items-center justify-center gap-y-8">
+        <a class="text-7xl text-white font-bold md:text-9xl text-center"
+            >TicTac<wbr />Go!</a
+        >
+        <div class="flex">
+            <button
+                class="bg-sky-400 px-[1.0185rem] py-2 rounded-md mx-2 hover:bg-sky-600 duration-300"
+            >
+                Sign Up
+            </button>
+            <button
+                class="bg-sky-400 px-[1.575rem] py-2 rounded-md mx-2 hover:bg-sky-600 transition-colors duration-300"
+            >
+                Login
+            </button>
+        </div>
+    </div>
+</template>
+<style>
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100dvh;
+}
+</style>
